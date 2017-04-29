78192ff1528d28caabbc2698ebf972c0f4df3fd3
terenty <terenty@list.ru>
2017-04-29 13:28:28 +0300

    task3_py

--- le.py
+++ le.py
@@ -10,3 +10,20 @@ def f(x, y):
 print(f(6, 3))
 print(f(6, 0))
 print(f(6, []))
+
+#--------------------------------
+def greet(name):
+    if name[0].isupper():
+        return "Hello, " + name
+    else:
+        raise ValueError(name + "is unappropriate name")
+
+while True:
+    try:
+        name = input("please enter your name:")
+        greeting = greet(name)
+        print(greeting)
+    except ValueError:
+        print("Please try again")
+    else:
+        break
--- lin.py
+++ lin.py
@@ -1,8 +1,16 @@
-try:
-    foo()
-except ZeroDivisionError:
-    print("ZeroDivisionError")
-except ArithmeticError:
-    print("ArithmeticError")
-except AssertionError:
-    print("AssertionError")
+class NonPositiveError(Exception):
+    pass
+
+class PositiveList(list):
+    def append(self, x):
+        if x <= 0:
+            raise NonPositiveError
+        else:
+            super().append(x)
+
+ls=PositiveList()
+
+ls.append(1)
+ls.append(2)
+print(ls)
+ls.append(-1)
