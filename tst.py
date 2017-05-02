431b1f740e77b65d0f9c9b583e44cd358847cf12
terenty <terenty@list.ru>
2017-05-02 14:16:30 +0300

    commit_file1

--- tst.py
+++ tst.py
@@ -25,14 +25,45 @@ import simplecrypt
 #     except simplecrypt.DecryptionException:
 #         print("Wrong password")
 
-with open("test.txt") as f, open("tset.txt", "w") as f1:
-    sp = []
-    for line in f:
-        sp.append(line.rstrip())
+#-------------files-------------------------
+# with open("test.txt") as f, open("tset.txt", "w") as f1:
+#     sp = []
+#     for line in f:
+#         sp.append(line.rstrip())
+#
+#     print(sp)
+#     sp.reverse()
+#     print(sp)
+#
+#     for li in sp:
+#         f1.write(li+"\n")
 
-    print(sp)
-    sp.reverse()
-    print(sp)
+import os
+import os.path
+import shutil
 
-    for li in sp:
-        f1.write(li+"\n")
+arcf = "main.zip"
+shutil.unpack_archive(arcf)
+dirname = arcf.split(".")[0]
+
+print(dirname)
+
+dir_py = []
+for (current_dir, dirs, files) in os.walk(dirname):
+#    print(current_dir, dirs, files)
+    for i in files:
+#        print(files)
+        if i.lower().endswith(".py"):
+#            print(current_dir)
+            dir_py.append(current_dir)
+#            print(dir_py)
+            break
+with open("main_out.txt", "w") as fout:
+    for el in sorted(dir_py):
+        print(el, file=fout)
+
+
+# print(os.getcwd())
+# print(os.listdir())
+# print(os.listdir(".idea"))
+# print(os.path.abspath("tst.py"))
