4e5880a82eb861b878f67cba8b1516a01d0998e9
terenty <terenty@list.ru>
2017-05-02 16:52:42 +0300

    commit_file_ytb

--- le10.py
+++ le10.py
@@ -1,11 +1,29 @@
-class BadName(Exception):
-    pass
+# unique value in list
+# def unique(iterable, seen=None):
+#     seen = set(seen or []) #None --- falsy
+#     acc = []
+#     for item in iterable:
+#         if item not in seen:
+#             seen.add(item)
+#             acc.append(item)
+#     return acc
+# xs = [1, 1, 2, 2, 3]
+# print(unique(xs))
+# rec = (0,0),(1,1)
+# print(rec)
+# (x1,y1),(x2,y2) = rec
+# print(x2)
+#---------------- key agrs ---------------------
+# def runner(cmd, **kwargs):
+#     print("runner...")
+#     if kwargs.get("verbose", True):
+#         print("Logging enabled")
+#
+# runner("mysql", limit=42)
+# runner("mysql", **{"verbose": False})
+# runner("mysql", **{"verbose": True})
+# options = {"verbose": False}
+# runner("mysql", **options)
+# runner("mysql", **{"verbose": True})
 
 
-def greet(name):
-    if name[0].isupper():
-        return "Hello, " + name
-    else:
-        raise BadName(name + "is unappropriate name")
-
-print("Import is execution")
